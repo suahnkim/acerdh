1c55b07193e6c8208157ccde10adeef35d264651
Suah Kim <suahnkim@gmail.com>
2019-07-28 00:23:41 +0900

    update

--- compress.m
+++
@@ -1,6 +0,0 @@
-function y = compress(x)
-    xC=cell(2,1);
-    xC{1}=double(x);
-    yC =  Arith07(xC);
-    y = reshape(dec2bin(yC)-48,1,[]);
-end
--- decompress.m
+++
@@ -1,7 +0,0 @@
-function y = decompress(x)
-        
-    xC=bin2dec(char(reshape(x,length(x)/8,8)+48));
-    yC=Arith07(xC);
-	y=yC{1}';
-
-end

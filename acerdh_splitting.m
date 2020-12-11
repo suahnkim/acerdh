c6f8915dfb28ab01caaf32c142aa121f974a605e
Suah Kim <suahnkim@gmail.com>
2020-12-11 13:32:41 +0900

    logic implemented in opposite way
    made the format for list_P_L_d the same as list_P_L = ['Frequency' 'value']

--- acerdh_splitting.m
+++ acerdh_splitting.m
@@ -174,11 +174,11 @@ list_P_L=sort_combine_table(sort_combine_table(:,1)==sort_combine_table(1,1),:);
 list_P_L(list_P_L(:,2)>P_H,2)=list_P_L(list_P_L(:,2)>P_H,2)+1;
 
 %if there are multiple candidates, choose the one with less P_L-d
-list_P_L_d=[list_P_L(:,2) table(list_P_L(:,2)+1,1)];
-list_P_L=list_P_L(list_P_L_d(:,2)==max(list_P_L_d(:,2)),:);
+list_P_L_d=[ table(list_P_L(:,2)+1,1) list_P_L(:,2)];
+list_P_L=list_P_L(list_P_L_d(:,1)==min(list_P_L_d(:,1)),:);
 %if there are multiple candidates, choose the one that is furthest away
 %from P_H
-[~,index]=min(abs(list_P_L(:,2)-P_H));
+[~,index]=max(abs(list_P_L(:,2)-P_H));
 P_L=list_P_L(index,2);
 end
 
